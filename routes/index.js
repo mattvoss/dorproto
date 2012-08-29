cd28a04bf93a97b15009ac4d0c47fb77fe35d9c4
Jason Weaver <indyplanets@gmail.com>
2012-08-29 10:05:46 -0500

    minor tweaks to hp for presentation

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@
  * GET home page.
  */
 exports.index = function(req, res){
-  res.render('index', { title: 'Texas A&amp;M Division of Research', body_class: 'home' })
+  res.render('index', { title: 'Research at Texas A&amp;M University', body_class: 'home' })
 };
 
